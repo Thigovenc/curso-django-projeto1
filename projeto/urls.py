6acb3a0c0c086a13a1d8f40f92aaddbf01ea1ec7
thigovenc <thiagovenceslau1997@gmail.com>
2024-02-15 22:31:07 -0300

    modification to the file urls.py

--- projeto/urls.py
+++ projeto/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 
+# HTTP REQUEST
+def my_view(request):
+    ...
+    return HttpResponse('Uma linda String !!')
+    # return HTTP RESPONSE
+    
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('sobre/', my_view)
 ]
